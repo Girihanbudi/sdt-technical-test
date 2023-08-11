4196b94848febd58b4bfac48f8e537b8e8cf7679
girihanbudi <Ghanbudi@gmail.com>
2023-08-11 21:07:31 +0700

    added cron and restfull api registration

--- index.ts
+++ index.ts
@@ -1,15 +1,19 @@
-import express, { Express, Request, Response } from "express";
+import express, { Express } from "express";
 import dotenv from "dotenv";
+import { registerRestApi } from "./src/registar";
+import cron from "node-cron";
+import CronList from "./src/cronjob";
 
 dotenv.config();
+
+// Run a cron list
+CronList.forEach((setting) => cron.schedule(setting.format, setting.task));
 
 const app: Express = express();
 const port = process.env.APP_PORT;
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Express + TypeScript Server");
-});
+registerRestApi(app);
 
 app.listen(port, () => {
-  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+  console.log(`[server]: Server is running at http://localhost:${port}`);
 });
