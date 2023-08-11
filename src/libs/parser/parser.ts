d68164816da0fbf6f6f66b0538a395241bdd917b
girihanbudi <Ghanbudi@gmail.com>
2023-08-11 21:03:53 +0700

    added body parser lib

---
+++ src/libs/parser/parser.ts
@@ -0,0 +1,7 @@
+import bodyParser from "body-parser";
+
+// create application/json parser
+export const jsonParser = bodyParser.json();
+
+// create application/x-www-form-urlencoded parser
+export const urlencodedParser = bodyParser.urlencoded({ extended: false });
