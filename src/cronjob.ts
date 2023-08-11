7c962194f53887613a12ac71b14e60e16a3515b0
girihanbudi <Ghanbudi@gmail.com>
2023-08-11 21:06:47 +0700

    install event to cron

---
+++ src/cronjob.ts
@@ -0,0 +1,17 @@
+import { HappyBirthdayMailEvent } from "./event/usecase";
+
+export interface CronValue {
+  format: string;
+  task: () => void;
+}
+
+export const HappyBirthDayEventCron = (): CronValue => {
+  return {
+    format: "*/15 * * * * *", // run every 15 minutes
+    task: () => HappyBirthdayMailEvent({ hour: 9 }),
+  };
+};
+
+export const CronList: CronValue[] = [HappyBirthDayEventCron()];
+
+export default CronList;
